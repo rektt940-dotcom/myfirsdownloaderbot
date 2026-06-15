d9d3ec0e19da1b34ac252d5eeaf35ffe3dd75ef3
rektt940-dotcom <rektt940@gmail.com>
2026-06-15 17:24:47 +0500

    Add files via upload

--- keep_alive.py
+++ keep_alive.py
@@ -7,8 +7,11 @@ app = Flask('')
 def home():
     return "Bot is running!"
 
+import os
+
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
